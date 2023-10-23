d42bf45e4102fe2875d27034f664f8544b38623e
Juan Lizondo <j.lizondolancharro@gmail.com>
2023-10-23 19:35:12 +0000

    iniciallized

--- src/main.py
+++ src/main.py
@@ -1,2 +1,7 @@
 # Obligatorio: Generar una tabla usando Python con TODOS los ficheros (recursivamente) del workspace que contenga el nombre del fichero, el peso REAL y la última fecha de modificación.
 # Opcional: Hacer lo mismo que en la línea anterior pero en Bash Scripting y exportando un CSV
+
+import os
+ 
+ficheros = os.walk('/workspaces/JLL_exercise-terminal-challenge')
+print(len(list(ficheros)))
