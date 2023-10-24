a623aa3ff0b8b3bae688158839cba0aaa6efea2d
Juan Lizondo <j.lizondolancharro@gmail.com>
2023-10-24 10:12:39 +0000

    finished the extracting dates

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
 # Obligatorio: Generar una tabla usando Python con TODOS los ficheros (recursivamente) del workspace que contenga el nombre del fichero, el peso REAL y la última fecha de modificación.
 # Opcional: Hacer lo mismo que en la línea anterior pero en Bash Scripting y exportando un CSV
 
+#Importing the neccesary librarys 
 import os
- 
-ficheros = os.walk('/workspaces/JLL_exercise-terminal-challenge')
-print(len(list(ficheros)))
+from humanize import naturalsize
+import time
+import pandas as pd
+
+#Iterating on the location of the files:
+for root, dirs, files in os.walk('/workspaces/JLL_exercise-terminal-challenge'):
+   for file in files:
+#Extracting the data: name, size and modification date:
+        file_path = os.path.join(root, file) 
+        file_size = os.stat(file_path).st_size
+        file_date = os.path.getmtime(file_path)
+#Cleaning the data for visualization:
+        file_path = os.path.basename(file_path)
+        file_size = naturalsize(file_size)
+        file_date = time.ctime(file_date)
+
+table = pd.DataFrame[["File", "Size", "Modification Date"],[file_path,file_size,file_date]]
+print(table)
+
+
+
+    
+
